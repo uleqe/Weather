0e2800fd79a64f97bae612766b09f2ff1fb6efec
uleqe <59677660+uleqe@users.noreply.github.com>
2021-02-22 23:54:11 +0600

    Add files via upload

---
+++ WeatherApp-master/Weather/cells/TableViewCell.swift
@@ -0,0 +1,21 @@
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dayName: UILabel!
+    @IBOutlet weak var imageIcon: UIImageView!
+    @IBOutlet weak var nightTemp: UILabel!
+    @IBOutlet weak var dayTemp: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
